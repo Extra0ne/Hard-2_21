0bdcb81caa186ebd49807a4d91e49a817babe2c9
Vasya Bogatchuk <v380962437840@gmail.com>
2020-09-11 00:35:38 +0300

    no message

---
+++ Hard-2_21/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hard_2_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nums= 10;
+            int[] mass = new int[nums];
+            int[] result = new int[nums];
+            int numsUnic = 0;
+            Random rnd = new Random();
+            for (int i = 0; i < nums; i++)
+            {
+                mass[i] = rnd.Next(nums + nums);
+            }
+
+            Console.WriteLine("Рандомный массив: ");
+            for (int i = 0; i < nums; i++)
+            {
+                Console.Write(mass[i] + " ");
+            }
+
+            for (int i = 0; i < nums; i++)
+            {
+                if (!result.Contains(mass[i]))
+                    result[numsUnic++] = mass[i];
+            }
+
+            Array.Resize(ref result, numsUnic);
+            Console.WriteLine("\nМассив без повторений: ");
+            for (int i = 0; i < numsUnic; i++)
+            {
+                Console.Write(result[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
